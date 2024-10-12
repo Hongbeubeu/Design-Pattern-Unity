796ca2bed82a864362d6b8206d62bac1fb36ca20
hongbeubeu <hongbeubeu@gmail.com>
2024-10-12 14:00:29 +0700

    Update singleton logic

--- Assets/Scripts/Singleton/Singleton.cs
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -2,42 +2,52 @@
 
 namespace Singleton
 {
-    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
-        private static readonly object _lock = new();
+
+        // ReSharper disable once StaticMemberInGenericType
+        private static readonly object Lock = new();
+
+        // ReSharper disable once StaticMemberInGenericType
         private static bool _applicationIsQuitting;
+
+        // ReSharper disable once StaticMemberInGenericType
+        private static bool _isSetDontDestroyOnLoad;
 
         public static T Instance
         {
             get
             {
                 if (_applicationIsQuitting) return null;
-                lock (_lock)
+                lock (Lock)
                 {
-                    if (_instance == null)
+                    if (!_instance)
                     {
                         _instance = (T)FindObjectOfType(typeof(T));
                     }
 
-                    if (_instance == null)
+                    if (!_instance)
                     {
                         var singleton = new GameObject();
                         _instance = singleton.AddComponent<T>();
                         singleton.name = $"(singleton) {typeof(T)}";
+                        if (_isSetDontDestroyOnLoad) return _instance;
                         DontDestroyOnLoad(singleton);
                     }
 
                     else
                     {
+                        if (_isSetDontDestroyOnLoad) return _instance;
                         DontDestroyOnLoad(_instance.gameObject);
                     }
+
+                    _isSetDontDestroyOnLoad = true;
                 }
 
                 return _instance;
             }
         }
-
 
         /// <summary>
         /// Prevent any call from other class call Instance after the application is quitting
@@ -45,6 +55,16 @@ namespace Singleton
         private void OnDestroy()
         {
             _applicationIsQuitting = true;
+            _isSetDontDestroyOnLoad = false;
         }
+
+#if UNITY_EDITOR
+        public static void ResetInstanceForTest()
+        {
+            _instance = null;
+            _applicationIsQuitting = false;
+            _isSetDontDestroyOnLoad = false;
+        }
+#endif
     }
 }
